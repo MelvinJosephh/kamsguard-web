daa6db837a61c54823ce046af2f8e8b8592267f6
MelvinJosephh <collinsme.2000@gmail.com>
2024-10-09 08:21:38 +0300

    refactored

--- apps/kamsguard-server/models/connectedDevice.js
+++ apps/kamsguard-server/models/connectedDevice.js
@@ -5,7 +5,7 @@ const connectedDeviceSchema = new mongoose.Schema({
   siteId: { type: String, required: true },          // Outer site identifier (e.g., "Kamsware-FV3")
   deviceId: { type: String, required: true },        // Nested device identifier (e.g., "1-Kamsware-FV3")
   status: { type: String, required: true },          // Device status: 'Online' or 'Offline'
-  lastActiveTime: { type: string, required: true },    // Timestamp of the last activity
+  lastActiveTime: { type: Date, required: true },    // Timestamp of the last activity
   // Add other fields if necessary
 });
 
