899f0af346755e7454ba2d791b3f725643217320
MelvinJosephh <collinsme.2000@gmail.com>
2024-10-17 10:08:32 +0300

    fixing utc time variation

---
+++ apps/kamsguard-server/shared-maps/events.mapping.js
@@ -0,0 +1,10 @@
+// eventMapping.js
+module.exports = {
+  HIGH_MOVEMENT_ALARM: 'High Movement Detected',
+  SMOKE_ALARM: 'Smoke Detected',
+  FLAME_ALARM: 'Flame Detected',
+  THERMAL1_ALARM: 'Thermal Warning',
+  THERMAL2_ALARM: 'Thermal Anomaly Detected',
+  LOW_LIGHT_ALARM: 'Low contrast alert',
+  HIGH_LIGHT_ALARM: 'High contrast alert',
+};
---
+++ apps/kamsguard-server/shared-maps/time-zone.js
@@ -0,0 +1,7 @@
+const timeZoneMap = {
+    'Kamsware-Africa': 'Africa/Nairobi',
+    'Kamsware-Europe': 'Europe/Berlin',
+    'Kamsware-Asia': 'Asia/Tokyo',
+    // Add other siteIds with corresponding time zones
+  };
+  
